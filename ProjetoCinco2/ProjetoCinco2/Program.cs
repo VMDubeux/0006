9d23ecc824b962e870e1fc28598df0f448879197
Victor Dubeux <adv.victordubeux@gmail.com>
2023-01-27 19:08:29 -0300

    P5.2 C2 Envio definitivo de projeto

---
+++ ProjetoCinco2/ProjetoCinco2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio2
+{
+    internal class QuintoProjeto2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor do raio do círculo em m²:");
+            float raio = float.Parse(Console.ReadLine());
+            double area = (double)Math.PI * Math.Pow(raio, 2.0);
+            Console.WriteLine($"O valor da área é de: {area}m²!");
+        }
+    }
+}
